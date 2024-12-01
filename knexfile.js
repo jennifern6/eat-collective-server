ce8ecce7d5efc8109b7f35f04c5dbca746e81a1a
Jennifern6 <jennifer_n@live.ca>
2024-12-01 12:56:48 -0500

    updated migrations of posts and users from MySQL

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,16 @@
+// Import dotenv to process environment variables from `.env` file.
 import "dotenv/config";
-import knex from "knex";
 
-
-const knexConfig = {
-  client: 'mysql2',
+export default {
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    charset: "utf8",
+  },
+  migrations: {
+    directory: './migrations',  
   },
 };
-
-
-export const db = knex(knexConfig);
-
-
-
