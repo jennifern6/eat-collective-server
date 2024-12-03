23fc71700f831fa7c3e4d49accabc32fa3e639fa
Jennifern6 <jennifer_n@live.ca>
2024-12-03 09:40:14 -0500

    created new migration to increase password to 255 char

--- seeds/seed_users.js
+++ seeds/seed_users.js
@@ -1,11 +1,45 @@
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+
+// import usersData from "../seed-data/users.js";
+
+// export async function seed(knex) {
+//   await knex("users").del();
+//   await knex("users").insert(usersData);
+// };
+
+
+
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 
-import usersData from "../seed-data/users.js";
 
-export async function seed(knex) {
-  await knex("users").del();
-  await knex("users").insert(usersData);
-};
+const usersData = [
+    {
+      id: 1,
+      username: "Jen",
+      email: "jen@gmail.com",
+      password: "$2a$10$MTgo4mHRzqfDbWCvBcujH.7ZL5pzghjyDjjfgW5gewvj1K5PfjyvW",
+      img: '{"filename":"jen_profile_pic.png"}',
+    },
+    {
+      id: 2,
+      username: "Sophia",
+      email: "sophia@gmail.com",
+      password: "$2a$10$DFm52XkctHMYv7aT1vbMJeF8L92Jt1XmxMeFAQSzQeO4pXlHRWida",
+      img: '{"filename":"sophia_profile_pic.png"}',
+    },
+  ];
+  
+  export async function seed(knex) {
+    await knex("users").del();
+    
+
+    await knex("users").insert(usersData);
+  };
+  
